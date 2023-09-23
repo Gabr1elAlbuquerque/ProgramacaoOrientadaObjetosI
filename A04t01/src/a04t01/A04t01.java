b3a99b5a4a24f9c3ec1f9671fc5df42b5f45b150
Gabriel Albuquerque <96128995+Gabr1elAlbuquerque@users.noreply.github.com>
2023-09-23 13:15:07 -0300

    CalculadoraInicio

---
+++ A04t01/src/a04t01/A04t01.java
@@ -0,0 +1,10 @@
+package a04t01;
+
+public class A04t01 {
+
+    public static void main(String[] args) {
+        JanelaPrincipal a = new JanelaPrincipal();
+        a.setVisible(true);
+    }
+    
+}
