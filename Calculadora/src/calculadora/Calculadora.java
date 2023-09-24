b916f93df8538e50056202ca88af0d418c314471
Gabriel Albuquerque <96128995+Gabr1elAlbuquerque@users.noreply.github.com>
2023-09-24 08:55:43 -0300

    Inicio do Projeto Calculadora
    Neste commit, foi realizado o esboço da tela, adicionando todos os botões necessários para a calculadora funcionar.

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,10 @@
+package calculadora;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        JanelaPrincipal inicio = new JanelaPrincipal();
+        inicio.setVisible(true);
+    }
+    
+}
